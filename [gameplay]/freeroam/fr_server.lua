bc81e6cb3a7b097148419278fc9d1c82e3b06998
Shady1 <eolshady@gmail.com>
2026-04-25 00:30:37 +0300

    Skins.xml
    Fix duplicate/incorrect skin entries and validate missing IDs
    
    Removed duplicate mapping between ID 271 and 300
    Noted missing skin (ID 312 - psycho)
    Preparing for further validation and PR

--- [gameplay]/freeroam/fr_server.lua
+++ [gameplay]/freeroam/fr_server.lua
@@ -1,6 +1,14 @@
 local g_PlayerData = {}
 local g_VehicleData = {}
 
+local function normalizeSkinId(skinid)
+	if tonumber(skinid) == 271 then
+		return 300
+	end
+
+	return skinid
+end
+
 g_ArmedVehicles = {
 	[425] = true,
 	[447] = true,
@@ -286,6 +294,7 @@ addEventHandler('onPlayerGravInit', root,
 
 function setMySkin(skinid)
 	if not isElement(client) then return end
+	skinid = normalizeSkinId(skinid)
 	if getElementModel(client) == skinid then return end
 	if isPedDead(client) then
 		local x, y, z = getElementPosition(client)
@@ -314,9 +323,12 @@ function spawnMe(x, y, z)
 	end
 
 	if isPedTerminated(client) then
-		repeat until spawnPlayer(client, x, y, z, 0, math.random(9, 288))
+		local skinid
+		repeat
+			skinid = normalizeSkinId(math.random(9, 288))
+		until spawnPlayer(client, x, y, z, 0, skinid)
 	else
-		spawnPlayer(client, x, y, z, 0, getPedSkin(client))
+		spawnPlayer(client, x, y, z, 0, normalizeSkinId(getPedSkin(client)))
 	end
 
 	setCameraTarget(client, client)
